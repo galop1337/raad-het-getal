a776f857e56d7720db4d2c7fdfe4dcc7f1dcebc0
bjorn <bjornzworld@hotmail.com>
2020-10-18 15:23:45 +0200

    functionaliteit toegevoegd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'first-app';  
+  randomNumber: number =Math.floor(Math.random()*100);
+  aantalBeurten = 10;
+  verschil;  
+  
+
+  checkGok(value){
+    this.verschil = this.randomNumber - value;
+    this.aantalBeurten = this.aantalBeurten -1; 
+    if (this.verschil === 0)
+    {
+      this.aantalBeurten = 0
+    }
+         
+  }
+  refresh(){
+    window.location.reload();
+  }
 }
